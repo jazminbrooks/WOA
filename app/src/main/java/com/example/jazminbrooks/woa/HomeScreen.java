3a6492a3d6fc5b61f9b41acc00170939971dee13
Aaron <aaron.mccanty@gmail.com>
2016-02-24 16:34:08 -0500

    Added more Activities

--- app/src/main/java/com/example/jazminbrooks/woa/HomeScreen.java
+++ app/src/main/java/com/example/jazminbrooks/woa/HomeScreen.java
@@ -1,13 +1,15 @@
 package com.example.jazminbrooks.woa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class HomeScreen extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity implements android.view.View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,42 @@ public class HomeScreen extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button launchChatRoom = (Button)(findViewById(R.id.ChatRoomButton));
+        launchChatRoom.setOnClickListener(this);
+        Button launchExerciseDescriptions = (Button)(findViewById(R.id.ExerciseDescriptionsButton));
+        launchExerciseDescriptions.setOnClickListener(this);
+        Button launchLocationButton = (Button)(findViewById(R.id.LocationsButton));
+        launchLocationButton.setOnClickListener(this);
+        Button launchBuildAWorkoutButton = (Button)(findViewById(R.id.BuildAWorkoutButton));
+        launchBuildAWorkoutButton.setOnClickListener(this);
+        Button launchMyAccount = (Button)(findViewById(R.id.MyAccountButton));
+        launchMyAccount.setOnClickListener(this);
+        Button logout = (Button)(findViewById(R.id.LogoutButton));
+        logout.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.LogoutButton:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.MyAccountButton:
+                startActivity(new Intent(this, MyAccount.class));
+                break;
+            case R.id.ChatRoomButton:
+                startActivity(new Intent(this, ChatRooms.class));
+                break;
+            case R.id.BuildAWorkoutButton:
+                startActivity(new Intent(this, BuildAWorkout.class));
+                break;
+            case R.id.LocationsButton:
+                startActivity(new Intent(this, Locations.class));
+                break;
+            case R.id.ExerciseDescriptionsButton:
+                startActivity(new Intent(this, ExerciseDescriptions.class));
+                break;
+        }
+    }
 }
