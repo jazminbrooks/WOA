181187ec434532a6253860944d51806d15dae7b8
Aaron <aaron.mccanty@gmail.com>
2016-02-29 20:25:20 -0500

    Added Sign-up Functionality

--- app/src/main/java/com/example/jazminbrooks/woa/HomeScreen.java
+++ app/src/main/java/com/example/jazminbrooks/woa/HomeScreen.java
@@ -1,6 +1,7 @@
 package com.example.jazminbrooks.woa;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HomeScreen extends AppCompatActivity implements android.view.View.OnClickListener{
+
+    private TextView textGreeting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +45,13 @@ public class HomeScreen extends AppCompatActivity implements android.view.View.O
         launchMyAccount.setOnClickListener(this);
         Button logout = (Button)(findViewById(R.id.LogoutButton));
         logout.setOnClickListener(this);
+
+        textGreeting = (TextView)findViewById(R.id.GreetingText);
+        SharedPreferences prefs = getSharedPreferences("com.example.jazminbrooks.woa", MODE_PRIVATE);
+        String username  = prefs.getString("username", "user");
+        textGreeting.setText("Hello " + username + "! You have 2000 points!");
+
+
     }
 
     @Override
