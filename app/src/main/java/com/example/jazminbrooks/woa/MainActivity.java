482f72293f2555d98110effd81ed59afbd7c58cb
jazminbrooks <jazminbrooks@gmail.com>
2016-03-09 16:32:38 -0500

    added log.d msgs to display in logcat when android lifecycle methods are invoked

--- app/src/main/java/com/example/jazminbrooks/woa/MainActivity.java
+++ app/src/main/java/com/example/jazminbrooks/woa/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.jazminbrooks.woa;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,27 +9,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.jazminbrooks.woa.Data.ExerciseContent;
 import com.firebase.client.Firebase;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 // Max Testing Git Commit...
 
-public class MainActivity extends AppCompatActivity implements android.view.View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button button;
     private EditText editText;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient mClient;
+
+    private final static String TAG = "TestActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(TAG, "On Create .....");
+
 
         Firebase.setAndroidContext(this);
-        Firebase myFirebaseRef= new Firebase("https://vivid-inferno-8916.firebaseio.com/");
+        Firebase myFirebaseRef = new Firebase("https://vivid-inferno-8916.firebaseio.com/");
 
         ExerciseContent.updateItems(myFirebaseRef);
 
@@ -45,12 +59,14 @@ public class MainActivity extends AppCompatActivity implements android.view.View
         });
 
 
-        Button launchSignup = (Button)(findViewById(R.id.signupbutton));
+        Button launchSignup = (Button) (findViewById(R.id.signupbutton));
         launchSignup.setOnClickListener(this);
-        Button launchLogin = (Button)(findViewById(R.id.loginbutton));
+        Button launchLogin = (Button) (findViewById(R.id.loginbutton));
         launchLogin.setOnClickListener(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        mClient = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -75,7 +91,7 @@ public class MainActivity extends AppCompatActivity implements android.view.View
 
 
     @Override
-    public void onClick(android.view.View v) {
+    public void onClick(View v) {
         switch (v.getId()) {
             case R.id.loginbutton:
                 startActivity(new Intent(this, Login.class));
@@ -84,5 +100,48 @@ public class MainActivity extends AppCompatActivity implements android.view.View
                 startActivity(new Intent(this, Signup.class));
                 break;
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Log.d(TAG, "On Start .....");
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        mClient.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.jazminbrooks.woa/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(mClient, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        Log.d(TAG, "On Stop .....");
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.jazminbrooks.woa/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(mClient, viewAction);
+        mClient.disconnect();
     }
 }
